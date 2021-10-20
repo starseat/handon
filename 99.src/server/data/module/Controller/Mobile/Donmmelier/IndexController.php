007138d73407f1105b2c5f21d7af717572c14883
starseat <starseat3@gmail.com>
2021-10-20 13:54:10 +0900

    모바일용 서버 소스 추가

---
+++ 99.src/server/data/module/Controller/Mobile/Donmmelier/IndexController.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This is commercial software, only users who have purchased a valid license
+ * and accept to the terms of the License Agreement can install and use this
+ * program.
+ *
+ * Do not edit or add to this file if you wish to upgrade Godomall5 to newer
+ * versions in the future.
+ *
+ * @copyright ⓒ 2016, NHN godo: Corp.
+ * @link http://www.godo.co.kr
+ */
+namespace Controller\Mobile\Donmmelier;
+
+use Component\Member\MyPage;
+use Component\Donmmelier\Test;
+use Framework\Debug\Exception\AlertBackException;
+use Framework\Debug\Exception\RedirectLoginException;
+
+/**
+ * 한돈 소믈리에 자격 페이지
+ *  - 로그인 X
+ * @author jw.lee
+ */
+class IndexController extends \Controller\Mobile\Controller
+{
+
+    /**
+     * index
+     *
+     */
+    public function index()
+    {
+    }
+}
---
+++ 99.src/server/data/module/Controller/Mobile/Donmmelier/MypageController.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This is commercial software, only users who have purchased a valid license
+ * and accept to the terms of the License Agreement can install and use this
+ * program.
+ *
+ * Do not edit or add to this file if you wish to upgrade Godomall5 to newer
+ * versions in the future.
+ *
+ * @copyright ⓒ 2016, NHN godo: Corp.
+ * @link http://www.godo.co.kr
+ */
+namespace Controller\Mobile\Donmmelier;
+
+use App;
+use Globals;
+use Request;
+use Session;
+use Component\Member\MyPage;
+use Component\Member\Member;
+use Component\Donmmelier\DonSVC;
+use Component\Member\Util\MemberUtil;
+use Component\Donmmelier\Test;
+use Framework\Debug\Exception\AlertBackException;
+use Framework\Debug\Exception\RedirectLoginException;
+
+/**
+ * 한돈 소믈리에 마이 페이지
+ *  - 로그인 필요
+ * @author jw.lee
+ */
+class MypageController extends \Controller\Mobile\Controller
+{
+  /**
+   * index
+   *
+   */
+  public function index()
+  {
+    $session = \App::getInstance('session');
+    if (!$session->has(Member::SESSION_MEMBER_LOGIN)) {
+      $donSvc = \App::load('\\Component\\Donmmelier\\DonSVC');
+      $returnUrl = $donSvc->getLoginUrl();
+      $msg = 'alert(\'로그인이 필요합니다.\');parent.location.href=\'' . $returnUrl . '\';';
+      $this->js($msg);
+      exit;
+    }
+    
+    /* 회원 로그인 정보 */
+    $myPage = new MyPage();
+    $memberData = $myPage->myInformation();
+    $memNm = $memberData['memNm'];
+
+    $this->setData('member', json_encode($memberData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    $this->setData('member_name', $memNm);
+  }
+}
