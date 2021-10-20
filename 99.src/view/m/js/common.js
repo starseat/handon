42e802311ebc2040d85c50780dfe0c844556a35c
starseat <starseat3@gmail.com>
2021-10-20 13:53:16 +0900

    모바일용 페이지 추가

---
+++ 99.src/view/m/js/common.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+    AOS.init();
+
+    init();
+
+    var mql = window.matchMedia("screen and (max-width: 720px)");
+    if (mql.matches) {
+        console.log("화면의 너비가 720px 보다 작습니다.");
+    } else {
+        console.log("화면의 너비가 720px 보다 큽니다.");
+    }
+});
+
+$(window).resize(function() {});
+
+function init() {
+    initEvent();
+}
+
+function initEvent() {
+    // 메뉴 open
+    $('#btn-navi-menu-close').on('click', function() {
+        $('#main-menu-wrap').removeClass('gnb_open');
+    });
+
+    // 메뉴 close
+    $('#btn-navi-menu-open').on('click', function() {
+        $('#main-menu-wrap').addClass('gnb_open');
+    });
+
+    $('#snsShareBtn').on('click', function() {
+        if (confirm('다른사람에게 이 글을 추천하고 싶나요?')) {
+            copyToClipboard();
+        }
+    });    
+}
+
+// 준비중 팝업
+// 나중에 삭제 필요
+function popupOpen_preparing(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    $('.layer_popup_box').hide();
+    $('#preparing-popup').show();
+    $('.wrap').addClass('popup_open');
+}
+
+function popupClose() {
+    $('.wrap').removeClass('popup_open');
+}
+
+// function copyToClipboard() {
+//     var copyUrl = 'http://donmmelier.com/';
+
+//     var agent = navigator.userAgent.toLowerCase();
+//     if ((navigator.appName == 'Netscape' && agent.indexOf('trident') != -1) || (agent.indexOf("msie") != -1)) {
+//         // IE 용
+//         window.clipboardData.setData("Text", copyUrl);
+//     } else {
+//         navigator.clipboard.writeText(copyUrl)
+//             //     .then(() => {
+//             //     console.log("Text copied to clipboard...")
+//             // })
+//             //     .catch(err => {
+//             //     console.log('Something went wrong', err);
+//             // })
+//         ;
+//     }
+
+//     alert('링크가 복사되었습니다.');
+// }
+
+function copyToClipboard(e) {
+    var tempItem = document.createElement('input');
+
+    tempItem.setAttribute('type', 'text');
+    tempItem.setAttribute('display', 'none');
+
+    // let content = e;    
+    // if (e instanceof HTMLElement) {
+    //     content = e.innerHTML;
+    // }
+
+    let content = 'https://mall.han-don.com/donmmelier/index.php';
+
+    tempItem.setAttribute('value', content);
+    document.body.appendChild(tempItem);
+
+    tempItem.select();
+    document.execCommand('Copy');
+
+    tempItem.parentElement.removeChild(tempItem);
+
+    alert('링크가 복사되었습니다.');
+}
+
+function logout() {
+    location.href = 'https://mall.han-don.com/member/logout.php?tabst=handon';
+}
---
+++ 99.src/view/m/js/lecture.js
@@ -0,0 +1,121 @@
+function popupOpen_lecture(event, index, isOpened) {
+    if(event) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    $('.layer_popup_box').hide();
+    $('#lecture-popup').show();
+
+    $('#lecture-popup .btn_arrow_l').hide();
+    $('#lecture-popup .btn_arrow_r').hide();
+    $('#lecture-popup-btn-index-prev').val(0);
+    $('#lecture-popup-btn-index-next').val(0);
+
+    if(LECTURE_PLAYER != null) {
+        LECTURE_PLAYER.dispose();
+        LECTURE_PLAYER = null;
+        $('#lecture-player-box').append('<video id="lecture-player" class="video-js vjs-big-play-centerd vjs-fluid vjs-default-skin" webkit-playsinline></video>');
+    }
+
+    if (index == 1) {
+        initLecture1();
+    } else if (index == 2) {
+        initLecture2();
+    } else {
+        initLecture3();
+    }
+
+    if(!isOpened) {
+        $('.wrap').addClass('popup_open');
+    }    
+}
+
+let LECTURE_PLAYER = null;
+const VIDEO_OPTIONS = {
+    poster: "image", // 썸네일 이미지
+    controls: true, // 컨트롤바 제공 여부
+    preload: "auto",
+    playsinline: true, // 모바일 브라우저 환경의 경우 동영상 재생 시 모바일 내장 플레이어로 실행이 되기 때문에 웹페이지에서 인라인형태로 제공하고 싶은 경우 true로 설정
+    width: 540,
+    height: 320,
+    controlBar: {
+        playToggle: true, // 재생/일시정지
+        pictureInPictureToggle: false, // pip 모드
+        remainingTimeDisplay: true, // 남은시간
+        progressControl: true, // 영상 시간 조정 
+        qualitySelector: true,
+        timeControl: true,
+    }
+};
+
+function initLecture1() {
+    $('#lecture-popup .btn_arrow_r').show();
+    $('#lecture-popup .txt_red').text('1회차 교육');
+    $('#lecture-popup-btn-index-next').val(2);
+
+
+    VIDEO_OPTIONS.sources = [{
+        // src: "./video/test.mp4",
+        src: 'https://file.han-don.com/donmmelier/test.mp4', 
+        type: "video/mp4"
+    }];
+
+    
+    LECTURE_PLAYER = videojs('lecture-player', VIDEO_OPTIONS);
+    LECTURE_PLAYER.ready(function() {
+        // var duration_time = Math.floor(this.duration());
+        // this.on('timeupdate', function () {
+        //     var current_time = Math.floor(this.currentTime());
+        //     if (current_time > 0 && (current_time == duration_time)) {
+        //         alert('다봤당 - timeupdate');
+        //     }
+        // });
+
+        this.on('ended', function() {
+            alert('1번 다봤당 - ended');
+        });
+    });
+}
+
+function initLecture2() {
+    $('#lecture-popup .btn_arrow_l').show();
+    $('#lecture-popup .btn_arrow_r').show();
+    $('#lecture-popup .txt_red').text('2회차 교육');
+    $('#lecture-popup-btn-index-prev').val(1);
+    $('#lecture-popup-btn-index-next').val(3);
+
+
+    VIDEO_OPTIONS.sources = [{
+        // src: "./video/test.mp4",
+        src: 'https://file.han-don.com/donmmelier/test.mp4', 
+        type: "video/mp4"
+    }];
+
+    LECTURE_PLAYER = videojs('lecture-player', VIDEO_OPTIONS);
+    LECTURE_PLAYER.ready(function() {
+        this.on('ended', function() {
+            alert('2번 다봤당 - ended');
+        });
+    });
+}
+
+function initLecture3() {
+    $('#lecture-popup .btn_arrow_l').show();
+    $('#lecture-popup .txt_red').text('3회차 교육');
+    $('#lecture-popup-btn-index-prev').val(2);
+}
+
+function prevLecturePopup() {
+    var prevIndex = parseInt($('#lecture-popup-btn-index-prev').val(), 10);
+    popupOpen_lecture(null, prevIndex, true);
+}
+
+function nextLecturePopup() {
+    var nextIndex = parseInt($('#lecture-popup-btn-index-next').val(), 10);
+    if(nextIndex == 3) {
+        alert('3회차 교육은 준비중 입니다.');
+        return false;
+    }
+    popupOpen_lecture(null, nextIndex, true);
+}
