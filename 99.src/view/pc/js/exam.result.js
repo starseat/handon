1ae3f9e8d5fc9d62a0b7de56352bfae8a18cdd72
starseat <starseat3@gmail.com>
2021-11-01 00:44:38 +0900

    소스 최신화

--- 99.src/view/pc/js/exam.result.js
+++ 99.src/view/pc/js/exam.result.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
     var isPass = $('#hiddenIsPass').val();
     if (isPass == '1') {
+        $('#donmm-quiz-result-box').addClass('handon_quiz_pass');
         $('#donmm-quiz-result-box-succ').show();
     } else {
         var examNum = parseInt($('#hiddenExamNum').val(), 10);
         if (examNum > 1) {
+            $('#donmm-quiz-result-box').addClass('handon_quiz_last');
             $('#donmm-quiz-result-box-fail-all').show();
         } else {
+            $('#donmm-quiz-result-box').addClass('handon_quiz_score');
             $('#donmm-quiz-result-box-fail').show();
         }
     }
