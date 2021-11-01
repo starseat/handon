b98e054072aaf64afd120c76149ea770546df430
starseat <starseat3@gmail.com>
2021-11-01 15:21:47 +0900

    exam init  페이지 제거 exam_preview 로 바로 이동하도록 수정

--- 99.src/server/data/module/Controller/Front/Donmmelier/ExamInitController.php
+++ 99.src/server/data/module/Controller/Front/Donmmelier/ExamInitController.php
@@ -38,47 +38,52 @@ class ExamInitController extends \Controller\Front\Controller
    */
   public function index()
   {
-    $donSvc = \App::load('\\Component\\Donmmelier\\DonSVC');
-    $session = \App::getInstance('session');
-    if (!$session->has(Member::SESSION_MEMBER_LOGIN)) {
-      $returnUrl = $donSvc->getLoginUrl();
-      $msg = 'alert(\'로그인이 필요합니다.\');parent.location.href=\'' . $returnUrl . '\';';
-      $this->js($msg);
-      exit;
-    }
+    // init 페이지 불필요 바로 preview 로 이동
+    $msg = 'parent.location.href=\'exam_preview.php\';';
+    $this->js($msg);
+    exit;
+    
+    // $donSvc = \App::load('\\Component\\Donmmelier\\DonSVC');
+    // $session = \App::getInstance('session');
+    // if (!$session->has(Member::SESSION_MEMBER_LOGIN)) {
+    //   $returnUrl = $donSvc->getLoginUrl();
+    //   $msg = 'alert(\'로그인이 필요합니다.\');parent.location.href=\'' . $returnUrl . '\';';
+    //   $this->js($msg);
+    //   exit;
+    // }
 
-    /* 회원 로그인 정보 */
-    $myPage = new MyPage();
-    $memberData = $myPage->myInformation();
-    $memId = $memberData['memId'];
-    $memNm = $memberData['memNm'];
+    // /* 회원 로그인 정보 */
+    // $myPage = new MyPage();
+    // $memberData = $myPage->myInformation();
+    // $memId = $memberData['memId'];
+    // $memNm = $memberData['memNm'];
 
-    $regi_count = $donSvc->getCountDonmmRegisteredFromId($memId);
-    if($regi_count < 1) {
-      $msg = 'alert(\'수강 신청을 하신 후 이용 바랍니다.\');parent.location.href=\'./index.php\';';
-      $this->js($msg);
-      exit;
-    }
+    // $regi_count = $donSvc->getCountDonmmRegisteredFromId($memId);
+    // if($regi_count < 1) {
+    //   $msg = 'alert(\'수강 신청을 하신 후 이용 바랍니다.\');parent.location.href=\'./index.php\';';
+    //   $this->js($msg);
+    //   exit;
+    // }
 
-    $lnum = $donSvc->getLastLectureNumber($memId);
-    if($lnum < 3) {
-      $msg = 'alert(\'강의를 모두 보아야 이용 가능합니다.\');parent.location.href=\'./lecture.php\';';
-      $this->js($msg);
-      exit;
-    }
+    // $lnum = $donSvc->getLastLectureNumber($memId);
+    // if($lnum < 3) {
+    //   $msg = 'alert(\'강의를 모두 보아야 이용 가능합니다.\');parent.location.href=\'./lecture.php\';';
+    //   $this->js($msg);
+    //   exit;
+    // }
 
-    // 응시 X : 0
-    // 합격 : 1
-    // 불합격 - 응시1: -1
-    // 불합격 - 응시2: -2  // 다음기회 X
-    $retExam = $donSvc->checkExamPass($memId);
-    if($retExam == 1 || $retExam == -2) {
-      $msg = 'parent.location.href=\'./exam_result.php\';';
-      $this->js($msg);
-      exit;
-    }
+    // // 응시 X : 0
+    // // 합격 : 1
+    // // 불합격 - 응시1: -1
+    // // 불합격 - 응시2: -2  // 다음기회 X
+    // $retExam = $donSvc->checkExamPass($memId);
+    // if($retExam == 1 || $retExam == -2) {
+    //   $msg = 'parent.location.href=\'./exam_result.php\';';
+    //   $this->js($msg);
+    //   exit;
+    // }
 
-    $this->setData('last_lecture_num', $donSvc->getLastLectureNumber($memId));
-    $this->setData('member_name', $memNm); 
+    // $this->setData('last_lecture_num', $donSvc->getLastLectureNumber($memId));
+    // $this->setData('member_name', $memNm); 
   }
 }
